7472a89c1032dd90863126691979e1cf687d1f6b
Ode Akugbe <akugbeode@yahoo.com>
2018-06-21 14:16:36 +0100

    Completed app setup.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+// importing dev-dependencies
+const gulp = require('gulp');
+const sass = require ('gulp-sass');
+
+gulp.task('styles', ()=>{
+	return gulp.src('./sass/index.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', ()=>{
+	gulp.watch('.sass/**/*.scss', gulp.series(['styles']));
+});
+
+gulp.task('default', gulp.parallel(['styles', 'watch']))
